b79890bbb9e1d69c35b160847145c513c1bba1fc
Collin Holloway <collinjholloway@gmail.com>
2022-09-08 17:26:53 -0500

    corecting date

--- nth-challenge.js
+++ nth-challenge.js
@@ -1,6 +1,6 @@
 // Return the Nth Even Number
 
-// Example(Input --> Output)
+// Example(Input --> Output)s
 
 // 1 --> 0 (the first even number is 0)
 // 3 --> 4 (the 3rd even number is 4 (0, 2, 4))
