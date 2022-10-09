0b254533e012635088631ecda7646cd229ccb41e
Collin Holloway <collinjholloway@gmail.com>
2022-10-09 09:00:08 -0500

    new code challenege

---
+++ CC 2022-10-9.js
@@ -0,0 +1,15 @@
+// When provided with a number between 0-9, return it in words.
+
+// Input :: 1
+
+// Output :: "One".
+
+function switchItUp(number) {
+if (number === 1) {
+    return "One";
+} else if (number === 3) {
+    return "Three";
+} else {
+    return "Five";
+}
+}
