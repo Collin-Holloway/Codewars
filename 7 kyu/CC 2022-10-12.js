d2d1c7b578b8fb8ce0ea782cf119225666156b77
Collin Holloway <collinjholloway@gmail.com>
2022-10-12 19:27:55 -0500

    adjusted code

--- 7 kyu/CC 2022-10-12.js
+++ 7 kyu/CC 2022-10-12.js
@@ -4,6 +4,4 @@ We will consider a, e, i, o, u as vowels for this Kata (but not y).
 
 The input string will only consist of lower case letters and/or spaces. */
 
-function getCount(str) {
-    return str.match(/[aeiou]/gi).length
-}
+const getCount = (str) => (str.match(/[aeiou]/gi) || []).length
