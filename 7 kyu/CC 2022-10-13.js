00571b5b707f90a16368f50426f0d39ff4bad602
Collin Holloway <collinjholloway@gmail.com>
2022-10-13 19:53:26 -0500

    coding challenege

---
+++ 7 kyu/CC 2022-10-13.js
@@ -0,0 +1,7 @@
+/*In this kata you will create a function that takes a list of 
+non-negative integers and strings and returns a new list with the strings filtered out.*/
+
+function filter_list(l) {
+    const numbers = l.filter( element => typeof element === 'number')
+    return numbers  
+}
