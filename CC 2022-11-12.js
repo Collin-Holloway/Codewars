3e5ab60deabf93d666a4ed76e26da1714dcad052
Collin Holloway <collinjholloway@gmail.com>
2022-11-14 17:49:19 -0600

    corrected text

--- CC 2022-11-12.js
+++ CC 2022-11-12.js
@@ -1,4 +1,4 @@
-/*Given the a list of numbers, return a fixed list so that the values increment by 1 for each index from the minimum value up to the maximum value (both included). */
+/*Given the a list oef numbers, return a fixed list so that the values increment by 1 for each index from the minimum value up to the maximum value (both included). */
 function pipeFix(numbers){
 	let arr = [];
 	for (let i = numbers[0]; i <= numbers[numbers.length - 1]; i++) {
