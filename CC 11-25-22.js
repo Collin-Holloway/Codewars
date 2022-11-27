4a9ae56807b7f2c4413e65643e6f9ec8ff898871
Collin Holloway <collinjholloway@gmail.com>
2022-11-26 22:06:08 -0600

    corrected error

--- CC 11-25-22.js
+++ CC 11-25-22.js
@@ -1,4 +1,4 @@
-//Complete the method that takes a boolean value and return a "Yes" string for true, or a "No" string for false.
+//Complete the method that takes a boolean values and return a "Yes" string for true, or a "No" string for false.
 
 function boolToWord( bool ){
     if( bool === true){
